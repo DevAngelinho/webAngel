8c6edd15fdbd126e32f01e08866468cc03992893
PincayAlexander <jpincay8421@utm.edu.ec>
2024-07-25 15:58:01 -0500

    Crear manejadores de errores 404 y 500

--- nucleoangel/urls.py
+++ nucleoangel/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -14,4 +14,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
-
+# Configuración de vistas personalizadas para errores 404 y 500
+handler404 = 'nucleoangel.views.error_404_view'
+handler500 = 'nucleoangel.views.error_500_view'
--- nucleoangel/views.py
+++ nucleoangel/views.py
@@ -11,3 +11,8 @@ def index(request):
     productos = Producto.objects.all()
     return render(request, 'index.html', {'productos': productos})
 
+def error_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def error_500_view(request):
+    return render(request, '500.html', status=500)
